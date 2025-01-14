3a9371f4e8b37da2fdbe249df01af05a57e5a3ed
Tom Collis <tom.collis@gmail.com>
2025-01-14 15:16:09 +0000

    v1

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(new URL(import.meta.url));
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/meetingtimer', (req, res) => {
+  const { agenda, times } = req.query;
+  
+  if (!agenda || !times) {
+    return res.status(400).send('Missing agenda or times parameters');
+  }
+
+  const agendaItems = agenda.split(',');
+  const timeItems = times.split(',').map(Number);
+
+  if (agendaItems.length !== timeItems.length || agendaItems.length > 10 || agendaItems.length < 1) {
+    return res.status(400).send('Invalid number of agenda items or times');
+  }
+
+  res.render('timer', { agendaItems, timeItems });
+});
+
+app.listen(port, () => {
+  console.log(`AgendaKeeper running at http://localhost:${port}`);
+});
+
