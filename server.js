0f864f29c3e63288f5b0036e32eaa8d76e576052
Tom Collis <tom.collis@gmail.com>
2025-01-17 20:28:09 +0000

    cPanel fix #2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import fs from 'fs';
 
 const __filename = fileURLToPath(new URL(import.meta.url));
 const __dirname = dirname(__filename);
@@ -9,26 +10,34 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use(express.static(join(__dirname, 'public')));
-
-app.get('/meetingtimer', (req, res) => {
-  const { agenda, times } = req.query;
-  
-  if (!agenda || !times) {
-    return res.status(400).send('Missing agenda or times parameters');
+// v1 - app.use(express.static(join(__dirname, 'public')));
+// Custom middleware to serve static files
+app.use((req, res, next) => {
+  const filePath = join(__dirname, "public", req.path);
+  if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()) {
+    return res.sendFile(filePath);
   }
-
-  const agendaItems = agenda.split(',');
-  const timeItems = times.split(',').map(Number);
-
-  if (agendaItems.length !== timeItems.length || agendaItems.length > 10 || agendaItems.length < 1) {
-    return res.status(400).send('Invalid number of agenda items or times');
-  }
-
-  res.render('timer', { agendaItems, timeItems });
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`AgendaControl running at http://localhost:${port}`);
-});
+  app.get('/meetingtimer', (req, res) => {
+    const { agenda, times } = req.query;
+
+    if (!agenda || !times) {
+      return res.status(400).send('Missing agenda or times parameters');
+    }
+
+    const agendaItems = agenda.split(',');
+    const timeItems = times.split(',').map(Number);
+
+    if (agendaItems.length !== timeItems.length || agendaItems.length > 10 || agendaItems.length < 1) {
+      return res.status(400).send('Invalid number of agenda items or times');
+    }
+
+    res.render('timer', { agendaItems, timeItems });
+  });
+
+  app.listen(port, () => {
+    console.log(`AgendaControl running at http://localhost:${port}`);
+  });
 
